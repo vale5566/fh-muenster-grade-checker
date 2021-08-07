8035b103adbcf663b991e5bc915f8c0588a7d508
vale5566 <vale@supervale.de>
2021-08-08 00:08:28 +0200

    fix

--- app.js
+++ app.js
@@ -26,25 +26,37 @@ async function configureBrowser() {
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
     return page;
 }
 
 async function login(page) {
-    await page.goto('https://www.fh-muenster.de/myfh', { waitUntil: 'networkidle2' });
+    await page.goto('https://www.fh-muenster.de/myfh', { waitUntil: 'networkidle0' });
     await page.type('#username', config.id);
     await page.type('#password', config.password);
+    await page.click('.login-button', { waitUntil: 'networkidle0' });
     await delay(3000);
-    await page.click('.login-button', { waitUntil: 'networkidle2' });
-
-    await page.goto('https://www.fh-muenster.de/sso.php?filename=/myfh&lang=DE');
+    await page.goto('https://www.fh-muenster.de/myfh/meine-nachrichten.php');
 }
 
 async function getNewestMessage(page) {
-    await page.goto('https://www.fh-muenster.de/myfh/meine-nachrichten.php'), { waitUntil: 'networkidle2' };
-
+    await page.setJavaScriptEnabled(false);
+    await page.reload();
+    await delay(3000);
     let $ = cheerio.load(await page.content());
     let message = $('.textcutt').first();
-    return message.text().replace(/  +/g, '').replace(/(\r\n|\n|\r)/gm, "");
+    let res = message.text().replace(/  +/g, '').replace(/(\r\n|\n|\r)/gm, "");
+    //console.log(res);
+    return res;
+}
+
+async function checkGrades(page, message) {
+    let tempMessage;
+    tempMessage = await getNewestMessage(page);
+    if (message != tempMessage && tempMessage) {
+        message = tempMessage;
+        client.channels.cache.get('764064335600812056').send(message);
+    }
 }
 
 async function startTracking() {
@@ -53,18 +65,13 @@ async function startTracking() {
     let message = await getNewestMessage(page);
     //client.channels.cache.get('764064335600812056').send('Hello here!');
 
-    let tempMessage;
     //runs every 30 minutes in this config
-    let job = new CronJob('* */30 * * * *', async function () {
-        tempMessage = await getNewestMessage(page);
-        if (message != tempMessage) {
-            message = tempMessage;
-            //console.log(message);
-            client.channels.cache.get('764064335600812056').send(message);
-        }
+    let job = new CronJob('*/30 * * * *', function () {
+        checkGrades(page, message);
     }, null, true, null, null, true);
     job.start();
 }
 
 client.login(config.BOT_TOKEN);
 startTracking();
+
